22879833cde55e92db71e48b81ffbbd966d71783
zhangdi <zhangdi013@sina.com>
2024-06-06 15:39:41 +0800

    修改port_scanner

--- port_scanner/src/main.rs
+++ port_scanner/src/main.rs
@@ -1,7 +1,6 @@
 use std::{net::IpAddr, process::exit};
 
 use clap::Parser;
-use tokio::sync::mpsc;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -32,60 +31,23 @@ fn args_validate(args: &Args) {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     let args = Args::parse();
     // 参数校验
     args_validate(&args);
 
-    // 建立channel
-    let (rx, mut tx) = mpsc::channel(2);
-
-    // 建立发送端task
-    let sender_task = tokio::spawn(async move {
-        let mut tasks = vec![];
-
-        for port in args.port_start..=args.port_end {
-            // println!("? {}:{}", args.addr, port);
-            let rx = rx.clone();
-            let task = tokio::spawn(async move {
-                let scan_attempt = scan(args.addr, port, rx).await;
-
-                if let Err(err) = scan_attempt {
-                    eprintln!("error: {err}");
-                }
-            });
-
-            tasks.push(task);
+    for port in args.port_start..=args.port_end {
+        let scan_attempt = scan(args.addr, port).await;
+        if let Some((addr, port)) = scan_attempt {
+            println!("{}:{}", addr, port);
         }
-
-        for task in tasks {
-            task.await.unwrap();
-        }
-        drop(rx);
-    });
-
-    // 建立接收端task
-    let receiver_task = tokio::spawn(async move {
-        while let Some((addr, port)) = tx.recv().await {
-            println!("={}:{}", addr, port);
-        }
-    });
-
-    let _ = sender_task.await.unwrap();
-    let _ = receiver_task.await.unwrap();
-
-    Ok(())
+    }
 }
 
-async fn scan(
-    addr: IpAddr,
-    port: u16,
-    results_tx: mpsc::Sender<(IpAddr, u16)>,
-) -> Result<(), mpsc::error::SendError<(IpAddr, u16)>> {
+async fn scan(addr: IpAddr, port: u16) -> Option<(IpAddr, u16)> {
     let connection_attempt = tokio::net::TcpStream::connect((addr, port)).await;
     if let Ok(_open) = connection_attempt {
-        results_tx.send((addr, port)).await.unwrap();
-    };
-
-    Ok(())
+        return Some((addr, port));
+    }
+    return None;
 }
